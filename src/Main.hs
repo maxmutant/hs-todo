19be38a9670dd58d25f534dab707f31a2311dc86
Maximilian Mayer <max@maxmayer.xyz>
2020-05-21 23:42:30 +0200

    removed Setup.hs and updated main window

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+
+ui :: Widget ()
+ui =
+    withBorderStyle unicode $
+    borderWithLabel (str "Snake") (center (str "Hello"))
+
 main :: IO ()
-main = putStrLn "hello, world"
+main = simpleMain ui
