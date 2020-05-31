bc60bb6ce3ffcb8e1161dd76539fdbe124dbdd3c
Maximilian Mayer <max@maxmayer.xyz>
2020-06-01 00:47:47 +0200

    added bottom input field and focusring

--- src/TodoUI.hs
+++ src/TodoUI.hs
@@ -11,26 +11,36 @@ import Lens.Micro
 import Lens.Micro.TH
 
 import qualified Brick.AttrMap          as BA
+import qualified Brick.Focus            as BF
 import qualified Brick.Widgets.List     as BL
 import qualified Brick.Main             as BM
+import qualified Brick.Widgets.Edit     as BE
 import qualified Brick.Types            as BT
 import qualified Data.Vector            as Vec
 import qualified Graphics.Vty           as V
 import qualified TodoIO                 as IO
 import qualified TodoItem               as TI
 
+
+data Name = List
+          | Edit
+          deriving (Ord, Show, Eq)
+
 data St =
     St { _file :: String
-       , _itemFilter :: String
-       , _list :: BL.List () TI.TodoItem
+       -- , _itemFilter :: String
+       , _focusRing :: BF.FocusRing Name
+       , _list :: BL.List Name TI.TodoItem
+       , _edit :: BE.Editor String Name
        }
 makeLenses ''St
 
-drawUI :: St -> [BT.Widget ()]
+drawUI :: St -> [BT.Widget Name]
 drawUI st = [ui]
     where
+        f = st^.focusRing
         l = st^.list
-        -- filteredList = listFilterElements l (st^.itemFilter)
+        e = st^.edit
         label = str "hs-todo 1.0.0 - Your tasks (" <+> cur <+> str " of "
             <+> total <+> str ")"
         cur = case l^.BL.listSelectedL of
@@ -38,20 +48,12 @@ drawUI st = [ui]
                 Just i  -> str (show (i + 1))
         total = str $ show $ Vec.length $ l^.BL.listElementsL
         ui = vBox [ label
-                  , BL.renderList listDrawElement True l
+                  , BF.withFocusRing f (BL.renderList listDrawElement) l
+                  , BF.withFocusRing f (BE.renderEditor (str. unlines)) e
                   ]
 
--- listFilterElements :: BL.List () TI.TodoItem -> String -> BL.List() TI.TodoItem
--- listFilterElements l _ = do
---     let ti = Vec.toList $ l^.BL.listElementsL
---         x = ti
---     BL.list () (Vec.fromList x) 1
-
-listDrawElement :: (Show a) => Bool -> a -> BT.Widget ()
+listDrawElement :: (Show a) => Bool -> a -> BT.Widget Name
 listDrawElement _ l = str (show l)
-
-setFilter :: String -> String
-setFilter _ = "phone"
 
 handleWriteFile :: St -> IO St
 handleWriteFile st = do
@@ -60,15 +62,21 @@ handleWriteFile st = do
     IO.writeTodoFile path items
     return st
 
-appHandleEvent :: St -> BT.BrickEvent () e -> BT.EventM () (BT.Next St)
+appHandleEvent :: St -> BT.BrickEvent Name e -> BT.EventM Name (BT.Next St)
 appHandleEvent st (BT.VtyEvent e) =
-    case e of
-        V.EvKey V.KEsc [] -> BM.halt st
-        V.EvKey (V.KChar 'q') [] -> BM.halt st
-        V.EvKey (V.KChar ' ') [] -> BM.continue $ st & list %~ BL.listModify TI.toggleDone
-        V.EvKey (V.KChar 'w') [] -> BM.continue =<< liftIO (handleWriteFile st)
-        V.EvKey (V.KChar 'f') [] -> BM.continue $ st & itemFilter %~ setFilter
-        _ -> BM.continue =<< BT.handleEventLensed st list (BL.handleListEventVi BL.handleListEvent) e
+    case BF.focusGetCurrent (st^.focusRing) of
+      Just List -> case e of
+            V.EvKey V.KEsc [] -> BM.halt st
+            V.EvKey (V.KChar 'q') [] -> BM.halt st
+            V.EvKey (V.KChar ' ') [] -> BM.continue $ st & list %~ BL.listModify TI.toggleDone
+            V.EvKey (V.KChar 'w') [] -> BM.continue =<< liftIO (handleWriteFile st)
+            V.EvKey (V.KChar 'e') [] -> BM.continue $ st & focusRing %~ BF.focusNext
+            V.EvKey (V.KChar '/') [] -> BM.continue $ st & focusRing %~ BF.focusNext
+            _ -> BM.continue =<< BT.handleEventLensed st list (BL.handleListEventVi BL.handleListEvent) e
+      Just Edit -> case e of
+            V.EvKey V.KEsc [] -> BM.continue $ st & focusRing %~ BF.focusNext
+            _ -> BM.continue =<< BT.handleEventLensed st edit BE.handleEditorEvent e
+      Nothing -> BM.continue st
 appHandleEvent st _ = BM.continue st
 
 appAttrMap :: BA.AttrMap
@@ -77,7 +85,7 @@ appAttrMap = BA.attrMap V.defAttr
     , (BL.listSelectedAttr,    V.black `on` V.yellow)
     ]
 
-todoApp :: BM.App St e ()
+todoApp :: BM.App St e Name
 todoApp = BM.App { BM.appDraw = drawUI
                  , BM.appChooseCursor = BM.showFirstCursor
                  , BM.appHandleEvent = appHandleEvent
@@ -88,8 +96,10 @@ todoApp = BM.App { BM.appDraw = drawUI
 initialState :: String -> [TI.TodoItem] -> St
 initialState path todoItems =
     St path
-       ""
-       (BL.list () (Vec.fromList todoItems) 1)
+       -- ""
+       (BF.focusRing [List, Edit])
+       (BL.list List (Vec.fromList todoItems) 1)
+       (BE.editor Edit (Just 1) "")
 
 runMain :: String -> [TI.TodoItem] -> IO [TI.TodoItem]
 runMain path todoItems = do
